4af55e3ba3b3604aedac3769b541bf0dc8a8a2ba
gkrishnans <gokulakrishnanaji@gmail.com>
2021-11-08 09:08:49 +0530

    updating env variables

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
-url = "http://127.0.0.1:5000"
+import os
+#url = "http://127.0.0.1:5000"
+url = "http://13.250.35.58:80"
 UPLOAD_FOLDER = './images'
 DATA_FILE = './data/data.txt'
-MONGO_URL = "mongodb+srv://gokul:gokuldb@python-project.svaov.mongodb.net/imageServer?retryWrites=true&w=majority"
+#sDBNAME = os.getenv('db')
+USERNAME = os.getenv('name')
+PASSWORD = os.getenv('password')
+MONGO_URL = "mongodb+srv://"+USERNAME+":"+PASSWORD+"@python-project.svaov.mongodb.net/imageServer?retryWrites=true&w=majority"
